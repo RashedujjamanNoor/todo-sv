9cec3fa3aa4c258296f42e0e78bb5d20cecb7685
RashedujjamanNoor <noorrk042@gmail.com>
2025-06-25 22:12:16 +0600

    create put route and controller

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,14 +4,20 @@ const User = require("../model/UserModel");
 router.post("/create", async (req, res) => {
   const { name, email, age } = req.body;
   const user = new User({ name, email, age });
-  try {
-    await user
-      .save()
-      .then(() =>
-        res.status(200).json({ message: "User created successfully" })
-      );
-  } catch (error) {
-    console.log(error);
+  const existUser = await User.findOne({ email: email });
+
+  if (existUser) {
+    res.json({ message: "User already exist" });
+  } else {
+    try {
+      await user
+        .save()
+        .then(() =>
+          res.status(200).json({ message: "User created successfully" })
+        );
+    } catch (error) {
+      res.status(400).json({ message: "Server Error" });
+    }
   }
 });
 
@@ -20,7 +26,19 @@ router.get("/", async (req, res) => {
     const data = await User.find();
     res.status(200).send(data);
   } catch (error) {
-    console.log(error);
+    res.status(400).json({ message: "Server Error" });
+  }
+});
+
+router.put("/update/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const data = req.body;
+    await User.findByIdAndUpdate(id, data, { new: true }).then(
+      res.status(200).json({ message: "User update successfully" })
+    );
+  } catch (error) {
+    res.status(400).json({ message: "Server Error" });
   }
 });
 
