31305f7b93bcf63cc6b306229b884deba5d4dd99
RashedujjamanNoor <noorrk042@gmail.com>
2025-06-26 23:50:39 +0600

    change databse connection string

--- db/mongobd.js
+++ db/mongobd.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async (req, res) => {
   await mongoose
     .connect(
-      "mongodb+srv://admin:admin@cluster0.xbeo0bh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+      "mongodb+srv://admin2:admin@cluster0.xbeo0bh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     )
     .then((res) => console.log("db connected"));
 };
